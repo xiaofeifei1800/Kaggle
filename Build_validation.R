98d7339fea04d9f30b8eb248c346c0f3d94a5210
xiaofeifei <liguoxin93@gmail.com>
2016-07-19 18:59:37 -0700

    add buildvalidation scropt

--- Build_validation.R
+++ Build_validation.R
@@ -1,14 +1,12 @@
+# PB score 0.48964
 setwd("/Users/xiaofeifei/Downloads/data/grupo")
 library(data.table)
 # use for kable
 library(knitr)
-
+library(Metrics)
 print("Reading data")
 train <- fread("train.csv", 
                select = c('Semana','Cliente_ID', 'Producto_ID', 'Agencia_ID', 'Ruta_SAK', 'Demanda_uni_equil'))
-
-test <- fread("test.csv", 
-              select = c('id', 'Cliente_ID', 'Producto_ID', 'Agencia_ID', 'Ruta_SAK'))
 
 print("Computing means")
 #transform target variable to log(1 + demand) - this makes sense since we're 
@@ -17,7 +15,7 @@ train$log_demand = log1p(train$Demanda_uni_equil)
 
 # split training and validation set
 validate = train[Semana == 9]
-train = = train[Semana != 9]
+train = train[Semana != 9]
 
 # calculate mean
 mean_total <- mean(train$log_demand) #overall mean
@@ -29,7 +27,7 @@ mean_PR <- train[, .(MPR = mean(log_demand)), by = .(Producto_ID, Ruta_SAK)]
 mean_PCA <- train[, .(MPCA = mean(log_demand)), by = .(Producto_ID, Cliente_ID, Agencia_ID)]
 
 print("Merging means with training set")
-submit <- merge(validate, mean_PCA, all.x = TRUE, by = c("Producto_ID", "Cliente_ID", "Agencia_ID"))
+submit <- merge(train, mean_PCA, all.x = TRUE, by = c("Producto_ID", "Cliente_ID", "Agencia_ID"))
 submit <- merge(submit, mean_PR, all.x = TRUE, by = c("Producto_ID", "Ruta_SAK"))
 submit <- merge(submit, mean_P, all.x = TRUE, by = "Producto_ID")
 
@@ -37,11 +35,11 @@ print("fit the linear function")
 fit1 = lm(log_demand~MPCA,data = submit)
 fit2 = lm(log_demand~MPR,data = submit)
 fit3 = lm(log_demand~MP,data = submit)
-fit4 = lm(log_demand~rep(mean_total, dim(validate)[1]),data = submit)
+fit4 = lm(log_demand~rep(mean_total, dim(train)[1]),data = train)
 
 print("Merging means with test set")
 rm(submit)
-submit <- merge(test, mean_PCA, all.x = TRUE, by = c("Producto_ID", "Cliente_ID", "Agencia_ID"))
+submit <- merge(validate, mean_PCA, all.x = TRUE, by = c("Producto_ID", "Cliente_ID", "Agencia_ID"))
 submit <- merge(submit, mean_PR, all.x = TRUE, by = c("Producto_ID", "Ruta_SAK"))
 submit <- merge(submit, mean_P, all.x = TRUE, by = "Producto_ID")
 
@@ -51,10 +49,4 @@ submit[is.na(Pred)]$Pred <- expm1(submit[is.na(Pred)]$MPR)*fit2$coefficients[2]+
 submit[is.na(Pred)]$Pred <- expm1(submit[is.na(Pred)]$MP)*fit3$coefficients[2]+fit3$coefficients[1]
 submit[is.na(Pred)]$Pred <- expm1(mean_total)+fit4$coefficients[1]
 
-print("Write out submission file")
-# now relabel columns ready for creating submission
-setnames(submit,"Pred","Demanda_uni_equil")
-setwd("/Users/xiaofeifei/GitHub/Kaggle_Bimbo/submit")
-# Any results you write to the current directory are saved as output.
-write.csv(submit[,.(id,Demanda_uni_equil)],"submit_mean_by_Agency_Ruta_Client.csv", row.names = FALSE)
-print("Done!")
+rmsle(validate[,Demanda_uni_equil], submit[,Pred])
