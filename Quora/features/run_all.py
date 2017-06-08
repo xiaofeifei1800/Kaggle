2c17f6cf1f50b94c35c60f86edd565e5097f7a9c
xiaofeifei <liguoxin93@gmail.com>
2017-06-08 23:00:20 +0100

    add run all

---
+++ Quora/features/run_all.py
@@ -0,0 +1,34 @@
+import os
+
+#################
+## Preprocesss ##
+#################
+#### preprocess data
+cmd = "python ./clean_data.py"
+os.system(cmd)
+
+#######################
+## Generate features ##
+#######################
+#### basic feature
+cmd = "python ./base_feature.py"
+os.system(cmd)
+
+cmd = "python ./base_feature1.py"
+os.system(cmd)
+
+#### count feature
+cmd = "python ./count.py"
+os.system(cmd)
+
+#### distance feature
+cmd = "python ./distance.py"
+os.system(cmd)
+
+#### magic feature
+cmd = "python ./magic_feature.py"
+os.system(cmd)
+
+### russian feature
+cmd = "python ./russian feature.py"
+os.system(cmd)
